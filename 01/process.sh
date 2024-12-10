b92ec27ece7e9749c05cd00afeb77a5a1fc5cbc0
Albert Sharkis <sharkis@sharkis.org>
2024-12-09 18:55:50 -0700

    solved

--- 01/process.sh
+++ 01/process.sh
@@ -2,5 +2,7 @@
 
 # squish input spaces
 cat input.txt | tr -s ' ' > squish.txt
-paste <( cut -d' ' -f1 squish.txt | sort) <(cut -d' ' -f2 squish.txt| sort)
-
+# combine squish files
+paste <( cut -d' ' -f1 squish.txt | sort) <(cut -d' ' -f2 squish.txt| sort) > pasted.txt
+# calculate difference between columns (square root of the square for positive result) and add to overall sum
+awk -F'\t' '{ sum += sqrt(($1-$2)*($1-$2)) } END { print sum }' pasted.txt
